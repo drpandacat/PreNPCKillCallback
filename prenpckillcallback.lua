56d0a25f29bda64b936abd98b67856771c949c75
Kerkel <134325525+drpandacat@users.noreply.github.com>
2025-05-19 09:54:42 -0400

    Update prenpckillcallback.lua

--- prenpckillcallback.lua
+++ prenpckillcallback.lua
@@ -2,7 +2,7 @@
     Pre NPC kill callback by Kerkel!!!!
 ]]
 
-local VERSION = 1
+local VERSION = 1.1
 
 if PRE_NPC_KILL then
     if PRE_NPC_KILL.Internal.VERSION > VERSION then return end
@@ -23,6 +23,7 @@ PRE_NPC_KILL = RegisterMod("Pre NPC Kill Callback", 1)
 ---Parameters:
 ---* npc `EntityNPC`
 ---* source `EntityRef`
+---* frozen `boolean`
 ---* amt `number`
 ---* flags `DamageFlag`
 ---* delay `integer`
@@ -35,9 +36,7 @@ PRE_NPC_KILL.Internal.CallbackEntries = {
         CallbackPriority.DEFAULT,
         ---@param npc EntityNPC 
         function (_, npc)
-            local data = npc:GetData()
-            data.____PRE_NPC_KILL_STORED_DAMAGE = 0
-            data.____PRE_NPC_KILL_SKIP_NEXT = nil
+            npc:GetData().____PRE_NPC_KILL_STORED_DAMAGE = 0
         end,
     },
     {
@@ -54,16 +53,17 @@ PRE_NPC_KILL.Internal.CallbackEntries = {
 
             data.____PRE_NPC_KILL_STORED_DAMAGE = (data.____PRE_NPC_KILL_STORED_DAMAGE or 0) + amt
 
-            if npc.HitPoints - data.____PRE_NPC_KILL_STORED_DAMAGE > 0
-            or npc:HasEntityFlags(EntityFlag.FLAG_NO_DEATH_TRIGGER)
+            if npc.HitPoints - data.____PRE_NPC_KILL_STORED_DAMAGE > 0 then return end
+
+            local frozen = npc:HasEntityFlags(EntityFlag.FLAG_NO_DEATH_TRIGGER)
             or npc:HasEntityFlags(EntityFlag.FLAG_ICE)
             or npc:HasEntityFlags(EntityFlag.FLAG_FREEZE)
             or npc:HasEntityFlags(EntityFlag.FLAG_MIDAS_FREEZE)
-            or (source.Type == EntityType.ENTITY_TEAR and source.Entity:ToTear():HasTearFlags(TearFlags.TEAR_ICE)) then return end
+            or (source.Type == EntityType.ENTITY_TEAR and source.Entity:ToTear():HasTearFlags(TearFlags.TEAR_ICE))
 
             for _, v in ipairs(Isaac.GetCallbacks(PRE_NPC_KILL.ID)) do
                 if not v.Param or v.Param == npc.Type then
-                    local result = v.Function(v.Mod, npc, source, amt, flags, delay)
+                    local result = v.Function(v.Mod, npc, source, frozen, amt, flags, delay)
 
                     if result == true then
                         data.____PRE_NPC_KILL_SKIP_NEXT = true
